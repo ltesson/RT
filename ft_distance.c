5523a4500df406d8f030ac798b025b74dbd449de
Louis TESSON <ltesson@e1r2p14.42.fr>
2017-05-31 19:11:38 +0200

    petit nettoyage et mise a la norme

--- ft_distance.c
+++ ft_distance.c
@@ -1,33 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_distance.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ltesson <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/19 14:33:36 by ltesson           #+#    #+#             */
-/*   Updated: 2017/05/03 19:04:46 by ltesson          ###   ########.fr       */
+/*   Created: 2017/05/31 17:19:00 by ltesson           #+#    #+#             */
+/*   Updated: 2017/05/31 18:38:53 by ltesson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <math.h>
 #include "rt.h"
-#include "libft/libft.h"
 
-int		main(int ac, char **av)
+int		ft_distance(double a, double b, double c, t_rayon *ray)
 {
-	t_scene		*s;
+	double	det;
+	double	t1;
+	double	t2;
 
-	if (ac != 2)
-	{
-		ft_endl("usage: ./rt <filepath>");
+	det = b * b - 4 * a * c;
+	if (det < 0)
 		return (0);
-	}
-	ft_readfile(s, av[2]);
-	if (s == NULL)
-	{
-		ft_endl("file not found");
+	t1 = (-b + sqrt(det)) / (2 * a);
+	t2 = (-b - sqrt(det)) / (2 * a);
+	if (t1 < 0.01 && t2 < 0.01)
 		return (0);
-	}
-	ft_draw(s);
+	else if (t1 >= 0.01 && t2 >= 0.01)
+		ray->t = fmin(fmin(t1, t2), ray->t);
+	else
+		ray->t = fmin(fmax(t1, t2), ray->t);
+	if (ray->t == t1 || ray->t == t2)
+		return (1);
 	return (0);
 }
